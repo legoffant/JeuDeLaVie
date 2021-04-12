8b9f02973d8449904dd1111c469b7182bd5d8bc1
legoffant <legoff.ant@gmail.com>
2021-04-13 00:29:16 +0200

    preparation canevas

--- jeudelavie.c
+++ jeudelavie.c
@@ -1,16 +1,40 @@
+/*                                 *
+ *       * JEU DE LA VIE *         *
+ *                                 *
+ * Version sans moteur graphique   *
+ * dans un terminal                *
+ * Date de création: 12 Avril 2021 *
+ * Compilateur: GCC                *
+ * Editeur de texte: Vim           *
+ * Auteur: Anthony JR. Le Goff     * 
+ * email: legoff.ant@gmail.com     */
+
 #include<stdio.h>
+#include<stdlib.h> 
+
+// Etat des cellules
+#define ALIVE 1
+#define DEAD 0
+
+// Taille de la matrice constante
+#define LIGNE 80
+#define COLONNE 180
+
+/*                *
+ *  Prototype de  *
+ *  fonction      */
+
+
+
+
+/*                   *
+ * Boucle principale *
+ *                   */
+
 
 int main(void) {
 
-	printf("test");
 	return 0;
-
-	struct gens {
-
-		int age;
-		float taille;
-		char genre;
-	};
 	
 	
 }
